f2bc091b3336d2d4b939a15e1d5d799395b788e8
chrys <294931812@qq.com>
2019-06-27 16:41:50 +0800

    edit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './common/css/common.less'
 import './common/css/iconfont.css'
 import * as THREE from 'three'
 import OrbitControls from 'three-orbitcontrols'
-import {TweenMax, RoughEase, Power2, Power4, TimelineLite, TweenLite} from "gsap";
+import {TweenMax, RoughEase, Power2, Power4, TimelineLite, TweenLite} from "gsap"
 import Bird from './components/Bird'
 import Rabbit from './components/Rabbit'
 import Dragon from './components/Dragon'
@@ -15,92 +15,92 @@ import {TWOPI, HALFPI, FireColors, TextColors, CamaraInit, CamaraActive} from '.
 import './components/splitText'
 
 
-let scene, camera, renderer, isActive = false, sound;
+let scene, camera, renderer, isActive = false, sound
 
-scene = new THREE.Scene();
-scene.fog = new THREE.Fog(0x242426, 20, 400);
+scene = new THREE.Scene()
+scene.fog = new THREE.Fog(0x242426, 20, 400)
 
-camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 10, 400);
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 10, 400)
 camera.position.set(CamaraInit.x, CamaraInit.y, CamaraInit.z)
-camera.updateProjectionMatrix();
+camera.updateProjectionMatrix()
 
-renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x242426);
-renderer.toneMapping = THREE.LinearToneMapping;
+renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setClearColor(0x242426)
+renderer.toneMapping = THREE.LinearToneMapping
 
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-window.addEventListener('resize', function () {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}, false);
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}, false)
 
-document.body.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement)
 renderer.domElement.id = 'main'
 
 /*////////////////////////////////////////*/
-const orbit = new OrbitControls(camera, renderer.domElement);
-orbit.enableZoom = false;
-orbit.enablePan = false;
-orbit.autoRotate = true;
-orbit.autoRotateSpeed = 0.6;
-orbit.update();
+const orbit = new OrbitControls(camera, renderer.domElement)
+orbit.enableZoom = false
+orbit.enablePan = false
+orbit.autoRotate = true
+orbit.autoRotateSpeed = 0.6
+orbit.update()
 
-let listener = new THREE.AudioListener();
-camera.add(listener);
+let listener = new THREE.AudioListener()
+camera.add(listener)
 
-// create a global audio source
-sound = new THREE.Audio(listener);
+// // create a global audio source
+// sound = new THREE.Audio(listener);
+//
+// // load a sound and set it as the Audio object's buffer
+// let audioLoader = new THREE.AudioLoader();
+// audioLoader.load(require('./assets/audio/bgMusic.ogg'), function (buffer) {
+//     sound.setBuffer(buffer);
+//     sound.setLoop(true);
+//     sound.setVolume(0.5);
+// });
 
-// load a sound and set it as the Audio object's buffer
-let audioLoader = new THREE.AudioLoader();
-audioLoader.load(require('./assets/audio/bgMusic.ogg'), function (buffer) {
-    sound.setBuffer(buffer);
-    sound.setLoop(true);
-    sound.setVolume(0.5);
-});
-
-let bird = new Bird();
+let bird = new Bird()
 bird.position.set(-50, 5, 50)
 bird.scale.set(.2, .2, .2)
-bird.rotation.y = 2.15 * Math.PI / 3;
-scene.add(bird);
+bird.rotation.y = 2.15 * Math.PI / 3
+scene.add(bird)
 
 
 /*////////////////////////////////////////*/
 //Rabbit
-let rabbit = new Rabbit();
-let angular = -Math.PI / 2;
-rabbit.position.set(50, -5, 0);
-rabbit.rotation.y = angular;
+let rabbit = new Rabbit()
+let angular = -Math.PI / 2
+rabbit.position.set(50, -5, 0)
+rabbit.rotation.y = angular
 rabbit.scale.set(.8, .8, .8)
-scene.add(rabbit);
+scene.add(rabbit)
 
 /*////////////////////////////////////////*/
 
 /*////////////////////////////////////////*/
 //Dragon
-let dragon = new Dragon();
-dragon.position.set(-40, 10, -75);
+let dragon = new Dragon()
+dragon.position.set(-40, 10, -75)
 dragon.scale.set(.35, .35, .35)
-dragon.rotation.y = 0.4 * Math.PI / 3;
-scene.add(dragon);
+dragon.rotation.y = 0.4 * Math.PI / 3
+scene.add(dragon)
 /*////////////////////////////////////////*/
 
 /*////////////////////////////////////////*/
 
 
-var ambientLight = new THREE.AmbientLight(0x222222);
-scene.add(ambientLight);
+var ambientLight = new THREE.AmbientLight(0x222222)
+scene.add(ambientLight)
 
 
-let hemiLight = new THREE.HemisphereLight(0xEBF7FD, 0xEBF7FD, 0.2);
-hemiLight.position.set(0, 100, 0);
-scene.add(hemiLight);
+let hemiLight = new THREE.HemisphereLight(0xEBF7FD, 0xEBF7FD, 0.2)
+hemiLight.position.set(0, 100, 0)
+scene.add(hemiLight)
 
 /*////////////////////////////////////////*/
 
@@ -108,66 +108,66 @@ function noiseMap(size, intensity) {
     var canvas = document.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         width = canvas.width = size || 512,
-        height = canvas.height = size || 512;
+        height = canvas.height = size || 512
 
-    intensity = intensity || 120;
+    intensity = intensity || 120
 
     var imageData = ctx.getImageData(0, 0, width, height),
         pixels = imageData.data,
         n = pixels.length,
-        i = 0;
+        i = 0
 
-    while (i < n) {
-        pixels[i++] = pixels[i++] = pixels[i++] = Math.sin(i * i * i + (i / n) * Math.PI) * intensity;
-        pixels[i++] = 255;
+    while(i < n) {
+        pixels[i++] = pixels[i++] = pixels[i++] = Math.sin(i * i * i + (i / n) * Math.PI) * intensity
+        pixels[i++] = 255
     }
-    ctx.putImageData(imageData, 0, 0);
+    ctx.putImageData(imageData, 0, 0)
 
-    let sprite = new THREE.Texture(canvas);
-    sprite.needsUpdate = true;
+    let sprite = new THREE.Texture(canvas)
+    sprite.needsUpdate = true
 
-    return sprite;
+    return sprite
 }
 
-let noise = noiseMap(512, 60);
+let noise = noiseMap(512, 60)
 
 
 /*////////////////////////////////////////*/
 //Flame
-let flames = Array(5).fill(null);
+let flames = Array(5).fill(null)
 flames.forEach((flame, i) => {
-    flame = new Flame(FireColors[Math.floor(FireColors.length * Math.random())]);
-    flame.position.z = 9 * Math.cos((i / flames.length) * TWOPI) + Math.sin(Math.random());
-    flame.position.x = 9 * Math.sin((i / flames.length) * TWOPI) + Math.sin(Math.random());
-    flame.position.y = 14;
-    scene.add(flame);
+    flame = new Flame(FireColors[Math.floor(FireColors.length * Math.random())])
+    flame.position.z = 9 * Math.cos((i / flames.length) * TWOPI) + Math.sin(Math.random())
+    flame.position.x = 9 * Math.sin((i / flames.length) * TWOPI) + Math.sin(Math.random())
+    flame.position.y = 14
+    scene.add(flame)
 
-});
+})
 
-let fire;
+let fire
 
 
 /*////////////////////////////////////////*/
 
-let fireParticles;
+let fireParticles
 
 function makeFireParticles() {
 
-    let pointGeometry = new THREE.Geometry();
+    let pointGeometry = new THREE.Geometry()
 
     for (let i = 0; i < 20; i++) {
-        let vertex = new THREE.Vector3();
-        vertex.x = Math.random() * 16 - 8;
-        vertex.y = Math.random() * 60;
-        vertex.z = Math.random() * 16 - 8;
-        vertex._maxHeight = 50 + Math.random() * 10;
-        vertex._speed = 0.1 + Math.random() * 0.1;
-        pointGeometry.vertices.push(vertex);
+        let vertex = new THREE.Vector3()
+        vertex.x = Math.random() * 16 - 8
+        vertex.y = Math.random() * 60
+        vertex.z = Math.random() * 16 - 8
+        vertex._maxHeight = 50 + Math.random() * 10
+        vertex._speed = 0.1 + Math.random() * 0.1
+        pointGeometry.vertices.push(vertex)
     }
 
-    pointGeometry.verticesNeedUpdate = true;
-    pointGeometry.normalsNeedUpdate = true;
-    pointGeometry.computeFaceNormals();
+    pointGeometry.verticesNeedUpdate = true
+    pointGeometry.normalsNeedUpdate = true
+    pointGeometry.computeFaceNormals()
 
     let pointMaterial = new THREE.PointsMaterial({
         //size: 16,
@@ -177,40 +177,40 @@ function makeFireParticles() {
         depthTest: true,
         transparent: true,
         opacity: 0.4,
-    });
+    })
 
-    let particles = new THREE.Points(pointGeometry, pointMaterial);
-    scene.add(particles);
+    let particles = new THREE.Points(pointGeometry, pointMaterial)
+    scene.add(particles)
 
-    let count = 0;
-    return function () {
-        count += 0.01;
+    let count = 0
+    return function() {
+        count += 0.01
         particles.geometry.vertices.forEach((vertex, i) => {
-            vertex.x += Math.sin(count * 1.5 + i) * 0.1;
-            vertex.z += Math.cos(count * 1.5 + i) * 0.1;
-            vertex.y += vertex._speed;
+            vertex.x += Math.sin(count * 1.5 + i) * 0.1
+            vertex.z += Math.cos(count * 1.5 + i) * 0.1
+            vertex.y += vertex._speed
             if (vertex.y > vertex._maxHeight) {
-                vertex.y = 0;
+                vertex.y = 0
             }
-        });
-        particles.geometry.verticesNeedUpdate = true;
+        })
+        particles.geometry.verticesNeedUpdate = true
     }
 }
 
-fireParticles = makeFireParticles();
+fireParticles = makeFireParticles()
 
 
 /*////////////////////////////////////////*/
 // LOG
-let logs = Array(5).fill(null);
+let logs = Array(5).fill(null)
 logs.forEach((log, i) => {
-    log = new Log();
-    log.position.x = 15 * Math.sin((i / logs.length) * TWOPI) + Math.sin(Math.random());
-    log.position.y = 5;
-    log.position.z = 1;
-    log.rotation.z = HALFPI / 2;// * Math.sin(i+1);
-    scene.add(log);
-});
+    log = new Log()
+    log.position.x = 15 * Math.sin((i / logs.length) * TWOPI) + Math.sin(Math.random())
+    log.position.y = 5
+    log.position.z = 1
+    log.rotation.z = HALFPI / 2// * Math.sin(i+1);
+    scene.add(log)
+})
 
 /*////////////////////////////////////////*/
 
@@ -219,35 +219,35 @@ logs.forEach((log, i) => {
 
 function snowyGround() {
 
-    let geometry = new THREE.PlaneGeometry(1000, 1000, 22, 12);
+    let geometry = new THREE.PlaneGeometry(1000, 1000, 22, 12)
     for (let i = 0; i < geometry.vertices.length; i++) {
-        geometry.vertices[i].z = (Math.sin(i * i * i) + 1 / 2) * 3;
+        geometry.vertices[i].z = (Math.sin(i * i * i) + 1 / 2) * 3
     }
-    geometry.verticesNeedUpdate = true;
-    geometry.normalsNeedUpdate = true;
-    geometry.computeFaceNormals();
+    geometry.verticesNeedUpdate = true
+    geometry.normalsNeedUpdate = true
+    geometry.computeFaceNormals()
 
     let material = new THREE.MeshPhongMaterial({
         color: 0xFFFFFF,
         shininess: 60,
         bumpMap: noise,
         bumpScale: 0.025,
-    });
+    })
 
-    let plane = new THREE.Mesh(geometry, material);
-    plane.rotation.x = Math.PI / -2;
-    plane.receiveShadow = true;
-    plane.position.y = -5;
+    let plane = new THREE.Mesh(geometry, material)
+    plane.rotation.x = Math.PI / -2
+    plane.receiveShadow = true
+    plane.position.y = -5
 
-    return plane;
+    return plane
 
 }
 
-scene.add(snowyGround());
+scene.add(snowyGround())
 
 /*////////////////////////////////////////*/
 
-let trees = [];
+let trees = []
 
 let rounds = [{
     x: 280,
@@ -258,13 +258,13 @@ let rounds = [{
 
 for (let k = 0; k < rounds.length; k++) {
     for (let i = 0; i < rounds[k].count; i++) {
-        let tree = new Tree(rounds[k].size);
-        tree.makeTree();
-        tree.scale.set(3.25, 3.25, 3.25);
-        tree.position.x = Math.sin(i + Math.random() * 0.2) * rounds[k].x;//(treeCount/2 - i) * 30;
-        tree.position.z = Math.cos(i + Math.random() * 0.1) * rounds[k].z;
-        trees.push(tree);
-        scene.add(tree);
+        let tree = new Tree(rounds[k].size)
+        tree.makeTree()
+        tree.scale.set(3.25, 3.25, 3.25)
+        tree.position.x = Math.sin(i + Math.random() * 0.2) * rounds[k].x//(treeCount/2 - i) * 30;
+        tree.position.z = Math.cos(i + Math.random() * 0.1) * rounds[k].z
+        trees.push(tree)
+        scene.add(tree)
     }
 }
 
@@ -275,43 +275,43 @@ for (let k = 0; k < rounds.length; k++) {
 
 function pointsParticles() {
 
-    let pointGeometry = new THREE.Geometry();
+    let pointGeometry = new THREE.Geometry()
 
     for (let i = 0; i < 10000; i++) {
-        let vertex = new THREE.Vector3();
-        vertex.x = Math.random() * 1000;
-        vertex.y = Math.random() * 500;
-        vertex.z = Math.random() * 1000;
-        pointGeometry.vertices.push(vertex);
+        let vertex = new THREE.Vector3()
+        vertex.x = Math.random() * 1000
+        vertex.y = Math.random() * 500
+        vertex.z = Math.random() * 1000
+        pointGeometry.vertices.push(vertex)
     }
 
     for (let i = 0; i < 10000; i++) {
-        let vertex = new THREE.Vector3();
-        vertex.x = -(Math.random() * 1000);
-        vertex.y = Math.random() * 500;
-        vertex.z = -(Math.random() * 1000);
-        pointGeometry.vertices.push(vertex);
+        let vertex = new THREE.Vector3()
+        vertex.x = -(Math.random() * 1000)
+        vertex.y = Math.random() * 500
+        vertex.z = -(Math.random() * 1000)
+        pointGeometry.vertices.push(vertex)
     }
 
     for (let i = 0; i < 10000; i++) {
-        let vertex = new THREE.Vector3();
-        vertex.x = (Math.random() * 1000);
-        vertex.y = Math.random() * 500;
-        vertex.z = -(Math.random() * 1000);
-        pointGeometry.vertices.push(vertex);
+        let vertex = new THREE.Vector3()
+        vertex.x = (Math.random() * 1000)
+        vertex.y = Math.random() * 500
+        vertex.z = -(Math.random() * 1000)
+        pointGeometry.vertices.push(vertex)
     }
 
     for (let i = 0; i < 10000; i++) {
-        let vertex = new THREE.Vector3();
-        vertex.x = -(Math.random() * 1000);
-        vertex.y = Math.random() * 500;
-        vertex.z = (Math.random() * 1000);
-        pointGeometry.vertices.push(vertex);
+        let vertex = new THREE.Vector3()
+        vertex.x = -(Math.random() * 1000)
+        vertex.y = Math.random() * 500
+        vertex.z = (Math.random() * 1000)
+        pointGeometry.vertices.push(vertex)
     }
 
-    pointGeometry.verticesNeedUpdate = true;
-    pointGeometry.normalsNeedUpdate = true;
-    pointGeometry.computeFaceNormals();
+    pointGeometry.verticesNeedUpdate = true
+    pointGeometry.normalsNeedUpdate = true
+    pointGeometry.computeFaceNormals()
 
     let pointMaterial = new THREE.PointsMaterial({
         //size: 16,
@@ -320,77 +320,64 @@ function pointsParticles() {
         depthTest: true,
         transparent: true,
         opacity: 0.3,
-    });
+    })
 
-    let particles = new THREE.Points(pointGeometry, pointMaterial);
-    scene.add(particles);
+    let particles = new THREE.Points(pointGeometry, pointMaterial)
+    scene.add(particles)
 
-    let count = 0;
-    return function () {
-        count += 0.01;
+    let count = 0
+    return function() {
+        count += 0.01
         particles.geometry.vertices.forEach((vertex, i) => {
-            vertex.x += Math.sin(count + i) * 0.05;
-            vertex.z += Math.cos(count + i) * 0.05;
-            vertex.y -= 0.5;
+            vertex.x += Math.sin(count + i) * 0.05
+            vertex.z += Math.cos(count + i) * 0.05
+            vertex.y -= 0.5
             if (vertex.y < 0) {
-                vertex.y = 500;
+                vertex.y = 500
             }
-        });
-        particles.geometry.verticesNeedUpdate = true;
+        })
+        particles.geometry.verticesNeedUpdate = true
     }
 }
 
 
-let updateParticles;
-updateParticles = pointsParticles();
+let updateParticles
+updateParticles = pointsParticles()
 
-renderer.gammaInput = true;
-renderer.gammaOutput = true;
+renderer.gammaInput = true
+renderer.gammaOutput = true
 
 /*////////////////////////////////////////*/
 
-let count = 3;
+let count = 3
 
 function render() {
-    requestAnimationFrame(render);
-    count += 0.03;
-    orbit.update();
+    requestAnimationFrame(render)
+    count += 0.03
+    orbit.update()
 
     if (updateParticles) {
-        updateParticles();
+        updateParticles()
     }
     if (fireParticles) {
-        fireParticles(count);
+        fireParticles(count)
     }
     if (fire && fire.flicker) {
-        fire.flicker(count);
+        fire.flicker(count)
     }
 
     //rabbitmove();
 
-    rabbit.idle();
+    rabbit.idle()
 
-    dragon.idle();
+    dragon.idle()
 
-    bird.idle();
+    bird.idle()
 
-    renderer.toneMappingExposure = Math.pow(0.91, 5.0);
+    renderer.toneMappingExposure = Math.pow(0.91, 5.0)
 
-    renderer.render(scene, camera);
+    renderer.render(scene, camera)
 
-};
+}
 
-render();
-
-//页面控制
-$(function () {
-    $(document).on('click', '.icon-play', function () {
-        $(this).removeClass('icon-play').addClass('icon-pause');
-        sound.play();
-    });
-
-    $(document).on('click', '.icon-pause', function () {
-        $(this).removeClass('icon-pause').addClass('icon-play');
-        sound.pause();
-    });
-})
+render()
